4db97568129a60147e6c0b5fff2cad87de34055c
LuisFer <G.G.LuisFer@gmail.com>
2016-09-21 23:49:36 -0500

    Interfaces:
    -Tablas y Combobox

--- src/ventanaPrincipal.java
+++ src/ventanaPrincipal.java
@@ -9,17 +9,22 @@ import javax.swing.JButton;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
 import java.awt.GridBagLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTable;
+import javax.swing.JComboBox;
 
 public class ventanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tablaCSV;
+	private JTable tablaFrecuencia;
 
-	//Hola
-	//Hola, ¿como nos vamos a dividir esto?
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -37,14 +42,18 @@ public class ventanaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public ventanaPrincipal() {
+		setTitle("Proyecto MIneria");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 350);
 		
+		/*Barra de menú*/
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
+		/*Menú Archivo*/
 		JMenu mnArchivo = new JMenu("Archivo");
 		menuBar.add(mnArchivo);
+		/*Elementos del menú Archivo*/
 		
 		JMenuItem mntmAbrir = new JMenuItem("Leer");
 		mnArchivo.add(mntmAbrir);
@@ -54,20 +63,56 @@ public class ventanaPrincipal extends JFrame {
 		
 		JMenuItem mntmSalir = new JMenuItem("Salir");
 		mnArchivo.add(mntmSalir);
+		mntmSalir.addActionListener((ActionEvent event)->{
+			System.exit(0);
+		});
+		
+		/*Grid Principal*/
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(SystemColor.text);
-		contentPane.add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0};
-		gbl_panel.rowHeights = new int[]{0};
-		gbl_panel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		/*ComboboxClases*/
+		JComboBox comboBoxClase = new JComboBox();
+		
+		JScrollPane scrollPaneCSV = new JScrollPane();
+		
+		JScrollPane scrollPaneFrecuencia = new JScrollPane();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(22)
+							.addComponent(comboBoxClase, 0, 84, Short.MAX_VALUE)
+							.addGap(313)
+							.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 359, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(421)
+							.addComponent(scrollPaneFrecuencia, GroupLayout.DEFAULT_SIZE, 357, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(19)
+							.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(29)
+							.addComponent(comboBoxClase, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(84)))
+					.addGap(7)
+					.addComponent(scrollPaneFrecuencia, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE))
+		);
+		
+		tablaFrecuencia = new JTable();
+		scrollPaneFrecuencia.setViewportView(tablaFrecuencia);
+		
+		tablaCSV = new JTable();
+		scrollPaneCSV.setViewportView(tablaCSV);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
