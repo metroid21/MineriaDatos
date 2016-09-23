a62e26c4756963e04aa4ce6e45d4be57b3ad21b1
LuisFer <G.G.LuisFer@gmail.com>
2016-09-23 00:12:10 -0500

    Iconos

--- src/ventanaPrincipal.java
+++ src/ventanaPrincipal.java
@@ -16,11 +16,14 @@ import java.io.UnsupportedEncodingException;
 import java.awt.GridBagLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTable;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 public class ventanaPrincipal extends JFrame {
 
@@ -56,6 +59,11 @@ public class ventanaPrincipal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 350);
 		
+		/*Iconos*/
+		Icon iconoAbrir = new ImageIcon("src/iconoAbrir.png");
+		Icon iconoGuardar = new ImageIcon("src/iconoGuardar.png");
+		Icon iconoSalir = new ImageIcon("src/iconoSalir.png");
+		
 		/*Barra de menú*/
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -65,14 +73,23 @@ public class ventanaPrincipal extends JFrame {
 		menuBar.add(mnArchivo);
 		/*Elementos del menú Archivo*/
 		
-		JMenuItem mntmAbrir = new JMenuItem("Leer");
+		JMenuItem mntmAbrir = new JMenuItem("Leer",iconoAbrir);
 		mnArchivo.add(mntmAbrir);
-		
-		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+	
+		JMenuItem mntmGuardar = new JMenuItem("Guardar",iconoGuardar);
 		mnArchivo.add(mntmGuardar);
 		
-		JMenuItem mntmSalir = new JMenuItem("Salir");
+		JMenuItem mntmSalir = new JMenuItem("Salir",iconoSalir);
 		mnArchivo.add(mntmSalir);
+		
+		JMenu mnPreprocesamiento = new JMenu("Preprocesamiento");
+		menuBar.add(mnPreprocesamiento);
+		
+		JMenuItem mntmTablaDeFrecuencia = new JMenuItem("Tabla de Frecuencia");
+		mnPreprocesamiento.add(mntmTablaDeFrecuencia);
+		
+		JMenu mnDatamining = new JMenu("DataMining");
+		menuBar.add(mnDatamining);
 		//mntmSalir.addActionListener((ActionEvent event)->{
 			//System.exit(0);
 		//});
@@ -88,16 +105,31 @@ public class ventanaPrincipal extends JFrame {
 		JScrollPane scrollPaneCSV = new JScrollPane();
 		
 		JScrollPane scrollPaneFrecuencia = new JScrollPane();
+		
+		JLabel lblClase = new JLabel("Clase:");
+		
+		JButton btnAadir = new JButton("Añadir");
+		
+		JButton btnBorrar = new JButton("Borrar");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(22)
-							.addComponent(comboBoxClase, 0, 84, Short.MAX_VALUE)
-							.addGap(313)
-							.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 359, Short.MAX_VALUE))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(54)
+									.addComponent(lblClase)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(comboBoxClase, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(66)
+									.addComponent(btnAadir)
+									.addGap(18)
+									.addComponent(btnBorrar)))
+							.addGap(334)
+							.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(421)
 							.addComponent(scrollPaneFrecuencia, GroupLayout.DEFAULT_SIZE, 357, Short.MAX_VALUE)))
@@ -106,16 +138,20 @@ public class ventanaPrincipal extends JFrame {
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addGap(19)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(19)
-							.addComponent(scrollPaneCSV, GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(29)
-							.addComponent(comboBoxClase, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(84)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(comboBoxClase, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblClase))
+							.addPreferredGap(ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnAadir)
+								.addComponent(btnBorrar))
+							.addGap(30)))
 					.addGap(7)
-					.addComponent(scrollPaneFrecuencia, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE))
+					.addComponent(scrollPaneFrecuencia, GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE))
 		);
 		
 		tablaFrecuencia = new JTable();
