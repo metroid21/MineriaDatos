c8d42639ecc6567ca4ca9b86ae61385d9616644b
metroid21 <G.G.LuisFer@gmail.com>
2016-11-25 19:18:56 -0600

    Creación de la clase para las transformaciones

---
+++ src/CalculadorTransformaciones.java
@@ -0,0 +1,21 @@
+
+public class CalculadorTransformaciones {
+
+	private DatosCSV datos;
+	
+	public CalculadorTransformaciones(DatosCSV datosN){
+		this.datos=datosN;
+	}
+	
+	public void minMax(int min, int max){
+		
+	}
+	
+	public void zeroR(){
+		
+	}
+	
+	public void diez(){
+		
+	}
+}
